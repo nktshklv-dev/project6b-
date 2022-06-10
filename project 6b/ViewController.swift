bf4731b3f96846f3387dc4d3ff517e85f416aaf9
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-10 14:44:10 +0300

    Initial Commit

---
+++ project 6b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project 6b
+//
+//  Created by Nikita  on 6/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
