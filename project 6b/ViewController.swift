22d83ea717c4c0334e53207951f6988a36ef048a
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-11 12:50:57 +0300

    the same thing with anchors

--- project 6b/ViewController.swift
+++ project 6b/ViewController.swift
@@ -11,56 +11,58 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let label1 = UILabel()
-        label1.translatesAutoresizingMaskIntoConstraints = false
         label1.backgroundColor = .red
         label1.text = "THESE"
-        label1.sizeToFit()
+        label1.translatesAutoresizingMaskIntoConstraints = false
         
         let label2 = UILabel()
-        label2.translatesAutoresizingMaskIntoConstraints = false
-        label2.backgroundColor = .cyan
+        label2.backgroundColor = .orange
         label2.text = "ARE"
-        label2.sizeToFit()
+        label2.translatesAutoresizingMaskIntoConstraints = false
         
         let label3 = UILabel()
-        label3.translatesAutoresizingMaskIntoConstraints = false
         label3.backgroundColor = .yellow
         label3.text = "SOME"
-        label3.sizeToFit()
+        label3.translatesAutoresizingMaskIntoConstraints = false
         
         let label4 = UILabel()
-        label4.translatesAutoresizingMaskIntoConstraints = false
         label4.backgroundColor = .green
         label4.text = "AWESOME"
-        label4.sizeToFit()
+        label4.translatesAutoresizingMaskIntoConstraints = false
         
         let label5 = UILabel()
-        label5.translatesAutoresizingMaskIntoConstraints = false
-        label5.backgroundColor = .orange
+        label5.backgroundColor = .blue
         label5.text = "LABELS"
-        label5.sizeToFit()
+        label5.translatesAutoresizingMaskIntoConstraints = false
         
         
-        view.addSubview(label1)
-        view.addSubview(label2)
-        view.addSubview(label3)
-        view.addSubview(label4)
-        view.addSubview(label5)
-        
-        let viewDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
-        
-       
-        
-        for label in viewDictionary.keys{
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewDictionary))
+        for label in [label1, label2, label3, label4, label5]{
+            view.addSubview(label)
         }
         
-        
-        
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewDictionary))
-        
-       
+//        let labelsDictionary = ["label1":label1, "label2":label2, "label3":label3, "label4":label4, "label5":label5]
+//        let metrics = ["labelHeight": 88]
+//
+//        for label in labelsDictionary.keys{
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: labelsDictionary))
+//        }
+//
+//
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(labelHeight)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: labelsDictionary))
+        var previous: UILabel?
+        for label in [label1, label2, label3, label4, label5]{
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else{
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            previous = label
+        }
         
         
     }
